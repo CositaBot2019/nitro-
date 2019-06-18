881468d89adcfd2ed0481805b84ba7bd52d1d1bc
CositaBot2019 <51672858+CositaBot2019@users.noreply.github.com>
2019-06-18 11:37:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,94 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "$"
+var adminprefix = '$'
 
 
+//bc
+
+client.on("message", message => {
+    if (message.content.startsWith("$obc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}\n ${m}`);
+  })
+  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
+  message.delete();
+  };
+  });
+
+
+//bc online
+
+
+  var prefix = "$";
+
+  client.on("message", message => {
+  
+              if (message.content.startsWith(prefix + "bc")) {
+                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+    let args = message.content.split(" ").slice(1);
+    var argresult = args.join(' '); 
+    message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+   m.send(`${argresult}\n ${m}`);
+  })
+   message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` :mailbox:  عدد المستلمين `); 
+   message.delete(); 
+  };     
+  });
+
+client.on('message', message => {
+    var  user = message.mentions.users.first() || message.author;
+if (message.content.startsWith("$avatar")) {
+message.channel.send(`This avatar For ${user} link : ${user.avatarURL}`);
+}
+});
+
+client.on('ready',  () => {
+    console.log('تم تشغيل :Broadcast  ');
+    console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+    console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
+    console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
+  });
+
+  client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('$adminbc')){
+if(!message.author.id === '476185102922285066') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+  client.on('message', msg => {
+    if(msg.content === '$help')
+    msg.reply('Check Your DM :white_check_mark:')
+  });
+  
+  
+  client.on("message", message => {
+    if (message.content === "$help") {
+     const embed = new Discord.RichEmbed() 
+         .setColor("#00FF00")
+         .setThumbnail(message.author.avatarURL)
+         .setDescription(`**Help|هيلب
+
+       $obc | لأرسال برود كاست للكل
+
+       $bc  |  لأرسال برود كاست للأونلاين
+
+       $adminbc | برودكاست عادي
+
+       ** `)
+   message.author.sendEmbed(embed)
+   
+   }
+   });
 
 var prefix = '$'; //  nitro  هوا امر التشغيل  //Toxic Codes
  
@@ -7,5 +96,4 @@ var _0x8b3c=["\x64\x69\x73\x63\x6F\x72\x64\x2E\x6A\x73","\x72\x65\x61\x64\x79","
  
 //Toxic Codes   الكود تحطه عادي في اي مكان في البوت ويشتغل ما عليك من الرموز لغريبة
 
-
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.TOKEN);
